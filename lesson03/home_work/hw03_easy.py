916d944c787813637fbc4f9db9c9762b8c46b5a0
Unknown <markovnik.007@gmail.com>
2018-08-02 20:53:47 +0700

    Вторушин Марк Викторович ДЗ Урок3

--- lesson03/home_work/hw03_easy.py
+++ lesson03/home_work/hw03_easy.py
@@ -1,3 +1,4 @@
+__author__ = 'Вторушин Марк Викторович'
 # Задание-1:
 # Напишите функцию, округляющую полученное произвольное десятичное число
 # до кол-ва знаков (кол-во знаков передается вторым аргументом).
@@ -5,7 +6,18 @@
 # Для решения задачи не используйте встроенные функции и функции из модуля math.
 
 def my_round(number, ndigits):
-    pass
+    sdvig = pow(10, int(ndigits))
+    a = number * sdvig
+    b = int(a)
+    ostatok = a - b
+    if not (abs(ostatok) < 0.5):
+        if b > 0:
+            b += 1
+        else:
+            b -= 1
+
+
+    return b / sdvig
 
 
 print(my_round(2.1234567, 5))
@@ -19,8 +31,16 @@ print(my_round(2.9999967, 5))
 # Билет считается счастливым, если сумма его первых и последних цифр равны.
 # !!!P.S.: функция не должна НИЧЕГО print'ить
 
+
 def lucky_ticket(ticket_number):
-    pass
+    number = str(ticket_number)
+    if len(number) != 6: return False
+    oktet_l = 0
+    oktet_r = 0
+    for i in range(3):
+        oktet_l += int(number[i])
+        oktet_r += int(number[-i-1])
+    return oktet_l == oktet_r
 
 
 print(lucky_ticket(123006))
--- lesson03/home_work/hw03_normal.py
+++ lesson03/home_work/hw03_normal.py
@@ -1,27 +1,58 @@
+__author__ = 'Вторушин Марк Викторович'
 # Задание-1:
 # Напишите функцию, возвращающую ряд Фибоначчи с n-элемента до m-элемента.
 # Первыми элементами ряда считать цифры 1 1
 
 def fibonacci(n, m):
-    pass
+    a = []
+    first_n = 0
+    second_n = 1
+    for i in range(m + 1):
+        if i >= n: a.append(second_n)
+        first_n, second_n = second_n, second_n + first_n
+
+    return a
+print(fibonacci(1,10))
 
 # Задача-2:
 # Напишите функцию, сортирующую принимаемый список по возрастанию.
 # Для сортировки используйте любой алгоритм (например пузырьковый).
 # Для решения данной задачи нельзя использовать встроенную функцию и метод sort()
 
-
 def sort_to_max(origin_list):
-    pass
+    a = origin_list[:]
+    for i in range(1, len(origin_list)):
+        n = i
+        while (a[n] < a[n - 1]) and (n > 0):
+            a[n], a[n - 1] = a[n - 1], a[n]
+            n -= 1
 
-sort_to_max([2, 10, -12, 2.5, 20, -11, 4, 4, 0])
+    return a
+print(sort_to_max([2, 5, 15, 23, 12, 14, 0, 3, 1, 8, 7]))
 
 # Задача-3:
 # Напишите собственную реализацию стандартной функции filter.
 # Разумеется, внутри нельзя использовать саму функцию filter.
+
+def filter_i(x, y):
+    a = []
+    for i in y:
+        if x(i):
+            a.append(i)
+    return a
+
+print(filter_i(lambda x:x>=0, [-27, 5, 0, 27, -2, -8, 17]))
+print(filter_i(lambda x:x<=0, [-27, 5, 0, 27, -2, -8, 17]))
 
 
 # Задача-4:
 # Даны четыре точки А1(х1, у1), А2(x2 ,у2), А3(x3 , у3), А4(х4, у4).
 # Определить, будут ли они вершинами параллелограмма.
 
+def versh(a1, a2, a3, a4):
+    def koord(x, y):
+        return ((x[0]+y[0])/2,(x[1]+y[1])/2)
+    if a1 == a3 or a2 == a4: return False
+    return koord(a1, a3) == koord(a2, a4)
+print (versh((2, 5), (3, 7), (1, 2), (4, 3)))
+print (versh((5, 0), (7, 1), (2, 1), (0, 0)))
