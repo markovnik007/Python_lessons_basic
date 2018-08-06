0e583e920e8588bc788d727e9cf5121442adcc9f
Unknown <markovnik.007@gmail.com>
2018-08-06 20:21:41 +0700

    Вторушин Марк Викторович ДЗ Урок4

--- lesson04/home_work/hw04_normal.py
+++ lesson04/home_work/hw04_normal.py
@@ -1,8 +1,12 @@
+__author__ = "Вторушин Марк Викторович"
 # Задание-1:
 # Вывести символы в нижнем регистре, которые находятся вокруг
 # 1 или более символов в верхнем регистре.
 # Т.е. из строки "mtMmEZUOmcq" нужно получить ['mt', 'm', 'mcq']
 # Решить задачу двумя способами: с помощью re и без.
+
+import re
+import numpy as np
 
 line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'\
        'GIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLeclMwAoktKlfUBJHPsnawvjPhfgewVzK'\
@@ -19,6 +23,27 @@ line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'\
        'qHFjvihuNGEEFsfnMXTfptvIOlhKhyYwxLnqOsBdGvnuyEZIheApQGOXWeXoLWiDQNJFa'\
        'XiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQoiQ'\
        'zTYwZAiRwycdlHfyHNGmkNqSwXUrxGc'
+
+a = re.split(r"[ABCDEFGHIJKLMNOPQRSTUVWXYZ]+", line)
+a = np.array(a)      #Решил вывести как матрицу, а то неудобно читать результат
+print("Массив с re")
+print(a)
+
+# Без re
+result = ""
+alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+for i in line:
+    if i in alphabet:
+        result += " "
+    else:
+        result += i
+result = list(filter(len, result.split(" ")))
+result = np.array(result)
+print("Массив без re")
+print(result)
+if len(a) == len(result):
+    print("Результаты вывода равны!")   # Для наглядности добавил проверку, чтобы понять равны ли результаты задачи с re и без
+else: print("Что то не работает...")
 
 
 # Задание-2:
@@ -45,9 +70,17 @@ line_2 = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysm'\
        'JFaXiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQ'\
        'oiQzTYwZAiRwycdlHfyHNGmkNqSwXUrxGC'
 
+regexp = '[a-z][a-z] *[A-Z]*[A-Z][A-Z]'
+list_1 = re.findall(regexp, line_2)
+result = list(filter(None,[i[2:][:-2] for i in list_1]))
+result = np.array(result)
+print("Массив с re")
+print(result)
+
 # Задание-3:
 # Напишите скрипт, заполняющий указанный файл (самостоятельно задайте имя файла)
 # произвольными целыми цифрами, в результате в файле должно быть
 # 2500-значное произвольное число.
 # Найдите и выведите самую длинную последовательность одинаковых цифр
 # в вышезаполненном файле.
+
